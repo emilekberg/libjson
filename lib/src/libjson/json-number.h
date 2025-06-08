d4ce6b6dc7bc12da7cdce9797a5619a6c1d6c6eb
Emil Ekberg <ekberg.emil@gmail.com>
2025-06-08 11:58:18 +0200

    don't need the constexpr conversion for floats

--- lib/src/libjson/json-number.h
+++ lib/src/libjson/json-number.h
@@ -10,27 +10,28 @@ public:
   JSONNumber(const std::string &literal) : _literal(literal) {}
 
   template <typename T> T get() {
-    if constexpr (std::is_floating_point_v<T>) {
-      if constexpr (std::is_same_v<T, float>) {
-        return std::stof(_literal);
-      } else if constexpr (std::is_same_v<T, double>) {
-        return std::stod(_literal);
-      } else if constexpr (std::is_same_v<T, long double>) {
-        return std::stold(_literal);
-      }
-    } else if constexpr (std::is_integral_v<T>) {
-      T value;
-      const char *begin = _literal.data();
-      const char *end = _literal.data() + _literal.size();
+    // if constexpr (std::is_floating_point_v<T>) {
+    //   if constexpr (std::is_same_v<T, float>) {
+    //     return std::stof(_literal);
+    //   } else if constexpr (std::is_same_v<T, double>) {
+    //     return std::stod(_literal);
+    //   } else if constexpr (std::is_same_v<T, long double>) {
+    //     return std::stold(_literal);
+    //   }
+    // } else
+    // if constexpr (std::is_integral_v<T>) {
+    T value;
+    const char *begin = _literal.data();
+    const char *end = _literal.data() + _literal.size();
 
-      auto result = std::from_chars(begin, end, value);
-      if (result.ec != std::errc{}) {
-        throw std::invalid_argument("Invalid integer: " + _literal);
-      }
-      return value;
-    } else {
-      static_assert(always_false<T>, "Unsupported type");
+    auto result = std::from_chars(begin, end, value);
+    if (result.ec != std::errc{}) {
+      throw std::invalid_argument("Invalid integer: " + _literal);
     }
+    return value;
+    // } else {
+    //   static_assert(always_false<T>, "Unsupported type");
+    // }
   }
 
 private:
