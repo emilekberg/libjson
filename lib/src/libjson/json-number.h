108b979f62709d2680f0fe22f878d0b52932bb70
Emil Ekberg <ekberg.emil@gmail.com>
2025-06-08 10:00:36 +0200

    convert types generically

--- lib/src/libjson/json-number.h
+++ lib/src/libjson/json-number.h
@@ -1,36 +1,42 @@
 #pragma once
+#include <charconv>
+#include <stdexcept>
 #include <string>
+#include <type_traits>
 
 namespace libjson {
 class JSONNumber {
 public:
   JSONNumber(const std::string &literal) : _literal(literal) {}
 
-  template <typename T> T get();
+  template <typename T> T get() {
+    if constexpr (std::is_floating_point_v<T>) {
+      if constexpr (std::is_same_v<T, float>) {
+        return std::stof(_literal);
+      } else if constexpr (std::is_same_v<T, double>) {
+        return std::stod(_literal);
+      } else if constexpr (std::is_same_v<T, long double>) {
+        return std::stold(_literal);
+      }
+    } else if constexpr (std::is_integral_v<T>) {
+      T value;
+      const char *begin = _literal.data();
+      const char *end = _literal.data() + _literal.size();
+
+      auto result = std::from_chars(begin, end, value);
+      if (result.ec != std::errc{}) {
+        throw std::invalid_argument("Invalid integer: " + _literal);
+      }
+      return value;
+    } else {
+      static_assert(always_false<T>, "Unsupported type");
+    }
+  }
 
 private:
   std::string _literal;
-};
 
-template <> inline int JSONNumber::get<int>() { return std::stoi(_literal); }
-template <> inline long JSONNumber::get<long>() { return std::stol(_literal); }
-template <> inline long long JSONNumber::get<long long>() {
-  return std::stoll(_literal);
-}
-template <> inline unsigned long JSONNumber::get<unsigned long>() {
-  return std::stoul(_literal);
-}
-template <> inline unsigned long long JSONNumber::get<unsigned long long>() {
-  return std::stoull(_literal);
-}
-template <> inline float JSONNumber::get<float>() {
-  return std::stof(_literal);
-}
-template <> inline double JSONNumber::get<double>() {
-  return std::stod(_literal);
-}
-template <> inline long double JSONNumber::get<long double>() {
-  return std::stold(_literal);
-}
+  template <typename> static constexpr bool always_false = false;
+};
 
 } // namespace libjson
--- test/cases/parse_number.test.cpp
+++ test/cases/parse_number.test.cpp
@@ -13,12 +13,12 @@ TEST(parse, number_int) {
   EXPECT_EQ(value.get<int>(), 420);
 }
 
-// TEST(parse, number_uint16_t) {
-//   libjson::Tokenizer tokens = libjson::tokenize(R"(65535)");
-//   // we know first token is an array, so discard it.
-//   libjson::Token token = tokens.next();
-//   libjson::JSONValue value = libjson::parseNumber(token);
-//
-//   EXPECT_EQ(value.type, libjson::JSONValueType::NUMBER);
-//   EXPECT_EQ(value.get<uint16_t>(), 0xFFFF);
-// }
+TEST(parse, number_uint16_t) {
+  libjson::Tokenizer tokens = libjson::tokenize(R"(65535)");
+  // we know first token is an array, so discard it.
+  libjson::Token token = tokens.next();
+  libjson::JSONValue value = libjson::parseNumber(token);
+
+  EXPECT_EQ(value.type, libjson::JSONValueType::NUMBER);
+  EXPECT_EQ(value.get<uint16_t>(), 0xFFFF);
+}
